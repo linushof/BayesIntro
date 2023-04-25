0df9a55b8b7825adb9f2d4cd6095831ba9188e75
linushof <linus.hof@tum.de>
2023-04-25 18:00:26 +0200

    add script for session 2

---
+++ code/session_2_software.R
@@ -0,0 +1,46 @@
+# R 2.1. Code Arithmetic Expressions
+
+1+1 # addition
+1-1 # subtraction
+1*1 # multiplication
+1/1 # division
+
+## expressions where order of operations matter
+
+1+1/2 #  division prior addition
+(1+1)/2 # brackets prior the rest
+
+
+# R 2.2. Scalars
+
+scal_num <- 1
+scal_chr <- "a"
+scal_str <- "word"
+scal_log <- TRUE # logical/Boolean
+
+
+# R 2.3. Vectors
+
+x <- c(1, 2, 3, 4) # create a vector object x
+
+# R 2.4. Data Frames
+x <- c(1, 2, 3)
+y <- c(4, 5, 6)
+z <- c(7, 8, 9)
+dat <- data.frame(x, y, z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
