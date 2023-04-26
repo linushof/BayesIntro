4829e87c72fbaf0539a9ca71029b4c1ed9931173
linushof <linus.hof@tum.de>
2023-04-26 08:02:52 +0200

    comment code for session 2

--- code/session_2_basics.R
+++ code/session_2_basics.R
@@ -13,84 +13,98 @@
 
 # R 2.2. Scalars
 
-scal_num <- 1
+## creating scalar objects
+
+scal_num <- 1 # <- is the assignment operator
 scal_chr <- "a"
 scal_str <- "word"
 scal_log <- TRUE # logical/Boolean
 
-scal_num
+## operating on scalar objects
+
 scal_num + 1
 scal_num + scal_num
 scal_num + scal_num / 2
 (scal_num + scal_num) / 2
 
-scal_num
+## creating new from existing objects
+
 scal_num <- scal_num + 1  # overwriting
 scal_num
 scal_num2 <- scal_num + 1 # creating
 scal_num2
 
-scal_chr * 4
+## invalid operation
+
+scal_chr * 4 # check and try to understand error message
 
 # R 2.3. Vectors
 
-x <- c(1, 2, 3, 4) # create a vector object x
-x
-x*2
+x <- c(1, 2, 3, 4) # create a vector object x, c() combines inputs to vector
+
+## Vectorized operations: the same operation is applied to each element in a vector
+
+x*2 
+
+### Operating with multiple operators
+
 x <- c(1, 2, 3, 4) # vector x
 y <- c(1, 2, 3, 4) # vector y
-x*y
+x*y # element-wise operation: R applies a given operation to elements that occupy the same position in their vectors
 
-z <- c(1,2) # vector z
-x*z
+z <- c(1,2) 
+x*z # R repeates the shorter vector until it matches the length of the longer vector and then operates element-wise
 
 z <- rep(z, 2) # replicates and overwrite z
 z
 x*z
 
 # R 2.4. Data Frames
+
 x <- c(1, 2, 3)
 y <- c(4, 5, 6)
 z <- c(7, 8, 9)
-dat <- data.frame(x, y, z)
+dat <- data.frame(x, y, z) # data.frame() creates a data frame out of vector of the same length
 dat
 
 ID <- c("A", "B", "C")
-dat <- data.frame(ID, dat)
+dat <- data.frame(ID, dat) # add new variable to exisiting data frame
 dat
 
 x <- c(1, 2, 3)
 y <- c(4, 5, 6)
 z <- c(7, 8, 9)
 dat <- data.frame(x, y, z)
-dat*2
+
+dat*2 # operate on the entire data frame
+
+### Retrieve information from a data frame and operate on them
 
 dat$x # dollar notation with variable name
 dat[, 1] # matrix notation with column number
 dat[, "x"] # matrix notation with variable name
 
-dat[, c(1,2)]
+dat[, c(1,2)] # retrieve multiple columns
 dat[, c("x","y")]
 
 dat[1,1] # cell entry row 1 and column 1
 dat[1, "x"]
+
 dat[, 1] * 2
 
 a <- c(1,2,3)
 dat[, 1] * a 
-dat$x * a
 
 # adding
 dat[, "v"] <- dat[, 1] * a 
 dat
 
-
-# overriding
-dat[, 4] <- dat[, 4] / a
+dat[, 4] <- dat[, 4] / a # divides column 4 by vector a and overwrite the column with the results
 dat
 
 
-# Functions
+# 2.5 Functions
+
 summing <- # function name
   function(x){ # inputs 
     
@@ -105,32 +119,27 @@ summing <- # function name
 summing(x)
 sum(x)
 
+## combining operations of multiple functions
+
+### step-by-step
 x <- c(-1, -2, -3)
 sum_x <- sum(x)
 sum_x
 abs(sum_x)
+
+### at once 
 abs(sum(x))
 
 
-### Tidyverse
+# 2.6 Tidyverse
+
 install.packages("tidyverse")
 library("tidyverse")
 
+# Create a PAT in GitHub 
 
-
-### GitHub 
 install.packages("usethis")
 usethis::create_github_token()
 
 install.packages("gitcreds")
 gitcreds::gitcreds_set()
-
-
-
-
-
-
-
-
-
-
